cbe9f0fc54d9bd23564dc63f32e4ad62a5d60b5e
hanumant <hemantbudhlada@gmail.com>
2022-07-31 22:19:21 +0530

    Testcase 2 and 3 are done from my side

---
+++ src/demoPackage/Test2.java
@@ -0,0 +1,9 @@
+package demoPackage;
+
+public class Test2 {
+	public void Abc()
+	{
+		System.out.println("Hello this is test lead");
+	}
+
+}
---
+++ src/demoPackage/Test3.java
@@ -0,0 +1,11 @@
+package demoPackage;
+
+public class Test3 
+{
+	
+	public void display()
+	{
+		System.out.println("Hello this is test tester");
+	}
+
+}
